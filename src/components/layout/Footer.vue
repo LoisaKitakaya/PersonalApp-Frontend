bd15e770b49d354db08fcacc539b0c6ba9ac9ab4
Freedom Loisa <kitakayaloisa@gmail.com>
2022-04-22 06:05:57 +0300

    created components to use in project

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="footer has-background-primary-light">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Personal App</strong> by <a href="#">Freedom Loisa</a>. The
+        source code is licensed <a href="#">MIT</a>. Check it out on
+        <a href="#">GitHub</a>.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style lang="scss">
+$border-color: hsl(0, 0%, 71%);
+
+.footer {
+  border: 1px solid $border-color;
+  border-bottom-left-radius: 0.3rem;
+  border-bottom-right-radius: 0.3rem;
+}
+</style>
---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="tabs is-centered has-background-primary-light">
+    <ul>
+      <li v-bind:class="{ 'is-active': $store.state.homeIsActive }">
+        <a href="#" @click="homeChangeActive">Home</a>
+      </li>
+      <li v-bind:class="{ 'is-active': $store.state.todoIsActive }">
+        <a href="#" @click="todoChangeActive">Todo</a>
+      </li>
+      <li v-bind:class="{ 'is-active': $store.state.habitIsActive }">
+        <a href="#" @click="habitChangeActive">Habit</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  methods: {
+    homeChangeActive() {
+      this.$store.commit("setHomeIsActive", true);
+    },
+
+    todoChangeActive() {
+      this.$store.commit("setTodoIsActive", true);
+    },
+
+    habitChangeActive() {
+      this.$store.commit("setHabitIsActive", true);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+$border-color: hsl(0, 0%, 71%);
+
+.tabs {
+  padding: 0.25rem 0 0 0 !important;
+  margin: 0 !important;
+  border-left: 1px solid $border-color;
+  border-right: 1px solid $border-color;
+  border-top: 1px solid $border-color;
+  border-top-right-radius: 0.3rem;
+  border-top-left-radius: 0.3rem;
+}
+</style>
